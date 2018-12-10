4579b3de30ee4ac5b42f01f9e5a1f4e2201a0e07
Gordiig <gordiig@gmail.com>
2018-12-10 11:20:05 +0300

    Add online group

--- Swimple_Server/consumers.py
+++ Swimple_Server/consumers.py
@@ -1,14 +1,27 @@
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 import json
 
 
 class ChatConsumer(WebsocketConsumer):
+    ONLINE_GROUP_NAME = 'online'
+
     def connect(self):
         print('Connect')
+        async_to_sync(self.channel_layer.group_add)(
+            ChatConsumer.ONLINE_GROUP_NAME,
+            self.channel_name
+        )
+        print('Added to online')
         self.accept()
 
     def disconnect(self, code):
         print('Disconnect')
+        async_to_sync(self.channel_layer.group_discard)(
+            ChatConsumer.ONLINE_GROUP_NAME,
+            self.channel_name
+        )
+        print('Deleted from online')
 
     def receive(self, text_data):
         incame_data = json.loads(text_data)
